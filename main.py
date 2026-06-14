be37a3ed82ac1f31b74cfc33b5f8e3c9a85673c9
anushashivni <anush.shivni@gmail.com>
2026-06-14 19:21:41 +0530

    FAST API INITIAL Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import os
+
+app =FastAPI()
+
+@app.get('/')
+def home():
+    return ("Hi this home page")
+
+
+if __name__=='__main__':
+    port=int(os.getenv("PORT",8080))
+    import uvicorn
+    uvicorn.run(app, host = '0.0.0.0', port =port)
